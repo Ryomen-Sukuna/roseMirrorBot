4be7bdadcbf198e1acd3139fd23aeaa7c6c0cef8
Amarnathcjd <amarnathcharichilil@gmail.com>
2022-06-09 13:08:38 +0530

    ok

--- modules/downloader.py
+++ modules/downloader.py
@@ -1,11 +1,14 @@
 import asyncio
+from os import getenv
 
 from telethon import Button
-from .helpers import auth_chat_only, get_time_diff, run_shell, get_size, hnd
+from .helpers import auth_chat_only, run_shell, get_size, hnd
 from .db import add_download_to_db, get_download_list, remove_download_from_db
 from requests import get
 
 import aria2p as aria2
+
+PORT = int(getenv("PORT", 6800))
 
 
 def setup_aria():
@@ -13,12 +16,12 @@ def setup_aria():
         "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt"
     ).text.replace("\n\n", ",")
     trackers = f"[{trackers_list}]"
-    cmd = f"aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port=6800 --max-connection-per-server=10 --rpc-max-request-size=1024M --check-certificate=false --follow-torrent=mem --seed-time=600 --max-upload-limit=0 --max-concurrent-downloads=1 --min-split-size=10M --follow-torrent=mem --split=10 --bt-tracker={trackers} --daemon=true --allow-overwrite=true"
+    cmd = f"aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port={PORT} --max-connection-per-server=10 --rpc-max-request-size=1024M --check-certificate=false --follow-torrent=mem --seed-time=600 --max-upload-limit=0 --max-concurrent-downloads=1 --min-split-size=10M --follow-torrent=mem --split=10 --bt-tracker={trackers} --daemon=true --allow-overwrite=true"
     run_shell(cmd, wait=False)
 
 
 setup_aria()
-ARIA = aria2.API(aria2.Client(host="http://localhost", port=6800, secret=""))
+ARIA = aria2.API(aria2.Client(host="http://localhost", port=PORT, secret=""))
 
 
 def add_download(chat_id, url, path):
